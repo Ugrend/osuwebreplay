02d1f74ab5ab2a56a229a654b55be89e638f45cc
Ugrend <ajlmatheson0@hotmail.com>
2016-06-03 00:33:40 +1000

    mods

---
+++ src/js_src/osu/mods.js
@@ -0,0 +1,38 @@
+/**
+ * Created by ugrend on 2/06/2016.
+ */
+
+MODS = {
+    NONE: 0,
+    NO_FAIL: 1,
+    EASY: 2,
+    NO_VIDEO: 4,
+    HIDDEN: 8,
+    HARD_ROCK: 16,
+    SUDDEN_DEATH: 32,
+    DOUBLE_TIME: 64,
+    RELAX: 128,
+    HALF_TIME: 256,
+    NIGHT_CORE: 512,
+    FLASH_LIGHT: 1024,
+    AUTO_PLAY: 2048,
+    SPUN_OUT: 4096
+};
+
+MOD_MULTI = {
+    NONE: 1.0,
+    NO_FAIL: 0.5,
+    EASY: 0.5,
+    NO_VIDEO: 1,
+    HIDDEN: 1.06,
+    HARD_ROCK: 1.06,
+    SUDDEN_DEATH: 1,
+    DOUBLE_TIME: 1.12,
+    RELAX: 0,
+    HALF_TIME: 0.3,
+    NIGHT_CORE: 1.12,
+    FLASH_LIGHT: 1.12,
+    AUTO_PLAY: 0,
+    SPUN_OUT: 0.9
+
+};
