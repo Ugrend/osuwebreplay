8371a4ed7c59e5d98fb73916642442f37d9cccaf
Ugrend <ajlmatheson0@hotmail.com>
2016-06-07 23:30:46 +1000

    just some comment stuff

--- src/js_src/io/indexeddb.js
+++ src/js_src/io/indexeddb.js
@@ -1,3 +1,27 @@
 /**
  * Created by Ugrend on 6/06/2016.
  */
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+
+if (!window.indexedDB) {
+    console.log("no index db = no storage ")
+}
+
+
+
+/*
+things to store
+
+    key should be md5sum?
+
+
+beatmaps
+mp3s
+skin_assets (wav png etc)
+skins (ini)
+replays
+backgrounds
+ */
--- static/js/osureplay.js
+++ static/js/osureplay.js
@@ -327,7 +327,30 @@ else {
 /**
  * Created by Ugrend on 6/06/2016.
  */
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+
+if (!window.indexedDB) {
+    console.log("no index db = no storage ")
+}
+
+
+
+/*
+things to store
+
+    key should be md5sum?
+
+
+beatmaps
+mp3s
+skin_assets (wav png etc)
+skins (ini)
+replays
+backgrounds
+ */
 /**
  * Created by Ugrend on 2/06/2016.
  */
