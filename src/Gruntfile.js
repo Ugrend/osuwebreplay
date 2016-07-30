b6fd17d9424542df14104e046f6ab501917f3a29
Aaron <ajlmatheson0@hotmail.com>
2016-07-30 22:34:53 +1000

    missing dependancys

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -40,9 +40,5 @@ module.exports = function (grunt) {
             }
         }
     });
-
-
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('build', ['concat']);
 };
