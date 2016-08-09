ab476da8752a1ecbb9bbfe1f34d452d0d357b4e6
Aaron <ajlmatheson0@hotmail.com>
2016-08-09 17:22:03 +1000

    Fixed silly mistake with onsucess/error

--- src/js_src/webapi/audio.js
+++ src/js_src/webapi/audio.js
@@ -18,8 +18,12 @@ osu.webapi.audio = {
             var a = new Audio();
             var song = osu.settings.SETTINGS.asset_server +"/" + beatmap.song_md5sum;
             a.src = song;
-            a.onloadedmetadata = osu.webapi.audio.findAudio(beatmap, callback, true, true);
-            a.onerror = osu.webapi.audio.findAudio(beatmap, callback, true,false);
+            a.onloadedmetadata = function () {
+                osu.webapi.audio.findAudio(beatmap, callback, true, true);
+            };
+            a.onerror = function () {
+                osu.webapi.audio.findAudio(beatmap, callback, true,false);
+            } ;
             return;
         }
 
@@ -32,20 +36,20 @@ osu.webapi.audio = {
         }
 
 
-        if(osu.settings.SETTINGS.song_url && !songFound){
-            $.ajax({
-                url: APIURL + "beatmaps",
-                type: 'GET',
-                data: {id: beatmap.md5sum, validate_only: true},
-                dataType: 'json',
-                success: function (data) {
-                    if (data) {
-                        var beatmap_id = data.data.beatmap_id;
-                        beatmap.song = osu.settings.SETTINGS.song_url+ "/" +beatmap_id;
-                        callback(beatmap);
+        if(osu.settings.SETTINGS.song_url && (!(osu.settings.SETTINGS.asset_server && checkedLocal && songFound))){
+                $.ajax({
+                    url: APIURL + "beatmaps",
+                    type: 'GET',
+                    data: {id: beatmap.md5sum, validate_only: true},
+                    dataType: 'json',
+                    success: function (data) {
+                        if (data) {
+                            var beatmap_id = data.data.beatmap_id;
+                            beatmap.song = osu.settings.SETTINGS.song_url+ "/" +beatmap_id;
+                            callback(beatmap);
+                        }
                     }
-                }
-            });
+                });
         }
 
         if(!osu.settings.SETTINGS.song_url && !songFound){
