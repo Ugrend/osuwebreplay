3f0e35a87becbca781f63af486cc99f44f2aff0c
Aaron <aaron@promed.com.au>
2016-10-18 16:07:43 +1100

    Removed old unused values

--- src/js_src/osu/skins.js
+++ src/js_src/osu/skins.js
@@ -130,62 +130,6 @@ osu.skins = {
     },
 
 
-
-    //hitbursts
-    hit300: "data/hit300.png",
-    hit300g: "data/hit300g.png",
-    hit300k: "data/hit300k.png",
-    hit100: "data/hit100.png",
-    hit100k: "data/hit100k.png",
-    hit50: "data/hit50.png",
-    hit0: "data/hit0.png",
-
-    //Ranking Grades
-    ranking_XH: "data/ranking-XH.png",
-    ranking_SH: "data/ranking-SH.png",
-    ranking_X: "data/ranking-X.png",
-    ranking_S: "data/ranking-S.png",
-    ranking_A: "data/ranking-A.png",
-    ranking_B: "data/ranking-B.png",
-    ranking_C: "data/ranking-C.png",
-    ranking_D: "data/ranking-D.png",
-    ranking_XH_small: "data/ranking-XH.png",
-    ranking_SH_small: "data/ranking-SH.png",
-    ranking_X_small: "data/ranking-X.png",
-    ranking_S_small: "data/ranking-S.png",
-    ranking_A_small: "data/ranking-A.png",
-    ranking_B_small: "data/ranking-B.png",
-    ranking_C_small: "data/ranking-C.png",
-    ranking_D_small: "data/ranking-D.png",
-
-
-    //Interface
-    pause_replay: "data/pause-replay.png",
-    menu_back: "data/menu-back.png",
-
-    cursor: "data/cursor.png",
-    cursortrail: "data/cursortrail.png",
-    cursormiddle: "data/cursormiddle.png",
-    cursor_smoke: "data/cursor-smoke.png",
-
-
-    inputoverlay_key: "data/inputoverlay-key.png",
-    star: "data/star.png",
-
-    //Playfield
-    section_fail: "data/section-fail.png",
-    section_pass: "data/section-pass.png",
-    play_warningarrow: "data/play-warningarrow.png",
-    play_skip: "data/play-skip.png",
-
-    hitcircle: "data/hitcircle.png",
-    hitcicleoverlay: "data/hitcircleoverlay.png",
-    approachcircle: "data/approachcircle.png",
-    followpoint: "data/followpoint.png",
-    sliderfollowcircle: "data/sliderfollowcircle.png",
-    reversearrow: "data/reversearrow.png",
-    sliderscorepoint: "data/sliderscorepoint.png",
-
     sliderb: [
         "data/sliderb0.png",
         "data/sliderb1.png",
@@ -198,49 +142,6 @@ osu.skins = {
         "data/sliderb8.png",
         "data/sliderb9.png"
     ],
-
-    spinner_approachcircle: "data/spinner-approachcircle.png",
-    spinner_background: "data/spinner-background.png",
-    spinner_bottom: "data/spinner-bottom.png",
-    spinner_circle: "data/spinner-circle.png",
-    spinner_clear: "data/spinner-clear.png",
-    spinner_glow: "data/spinner-glow.png",
-    spinner_metre: "data/spinner-metre.png",
-    spinner_middle: "data/spinner-middle.png",
-    spinner_middle2: "data/spinner-middle2.png",
-    spinner_osu: "data/spinner-osu.png",
-    spinner_rpm: "data/spinner-rpm.png",
-    spinner_spin: "data/spinner-spin.png",
-    spinner_top: "data/spinner-top.png",
-    spinner_warning: "data/spinner-warning.png",
-
-
-
-    default_0: "data/default-0.png",
-    default_1: "data/default-1.png",
-    default_2: "data/default-2.png",
-    default_3: "data/default-3.png",
-    default_4: "data/default-4.png",
-    default_5: "data/default-5.png",
-    default_6: "data/default-6.png",
-    default_7: "data/default-7.png",
-    default_8: "data/default-8.png",
-    default_9: "data/default-9.png",
-    //Mods
-
-    selection_mod_doubletime: "data/selection-mod-doubletime.png",
-    selection_mod_easy: "data/selection-mod-easy.png",
-    selection_mod_flashlight: "data/selection-mod-flashlight.png",
-    selection_mod_halftime: "data/selection-mod-halftime.png",
-    selection_mod_hardrock: "data/selection-mod-hardrock.png",
-    selection_mod_hidden: "data/selection-mod-hidden.png",
-    selection_mod_nightcore: "data/selection-mod-nightcore.png",
-    selection_mod_nofail: "data/selection-mod-nofail.png",
-    selection_mod_perfect: "data/selection-mod-perfect.png",
-    selection_mod_spunout: "data/selection-mod-spunout.png",
-    selection_mod_suddendeath: "data/selection-mod-suddendeath.png",
-
-
 
     //AUDIO
 
